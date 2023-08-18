e28ee86f187116cf45a0c9c9830f3dee15912bbd
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-18 13:33:50 +0300

    Add project files.

---
+++ DateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TervehdysNimellä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kysytään käyttäjältä nimi ja tallennetaan se muuttujaan
+            Console.WriteLine("Syötä nimesi:");
+            string nimi = Console.ReadLine();
+
+            // Hae tämänhetkinen päivämäärä ja aika
+            DateTime nykyinenAika = DateTime.Now;
+
+            // Tulostetaan tervehdys käyttäjän nimellä ja nykyisellä päivämäärällä/ajalla
+            Console.WriteLine("Hei " + nimi + "! Tervetuloa " + nykyinenAika);
+
+            // Pysäytetään ohjelma odottamaan syötettä ennen sulkemista
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MATH/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sade = 5.0;
+
+            // Lasketaan ympyrän pinta-ala ja ympärysmitta
+            double pintaAla = Math.PI * Math.Pow(sade, 2);
+            double ymparysmitta = 2 * Math.PI * sade;
+
+            Console.WriteLine("Ympyrän pinta-ala: " + pintaAla);
+            Console.WriteLine("Ympärysmitta: " + ymparysmitta);
+
+            Console.ReadLine();
+        }
+    }
+}
+
